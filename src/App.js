7fdd6371d8e69892d107d2bd8221ff3bf2695272
Andrii Mishchenko <a.mishenko2001@gmail.com>
2021-06-08 19:48:03 +0300

    Steps 1-2 completed

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Feedback from './components/feedback/Feedback'
+
+import './index.css'
+
+
+const App = () =>{
+  return (
+      <>
+          <Feedback/>
+
+      </>
+  );
+};
+
+
+
+export default App;
